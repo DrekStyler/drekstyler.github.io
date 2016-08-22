812dae73cbe8bc5dc5aac0e46eb4242e31b3836d
Derek Styer <dmstyer@gmail.com>
2016-08-22 15:24:41 -0600

    adding post method

--- app/pages/orders.js
+++ app/pages/orders.js
@@ -6,63 +6,84 @@
   let item;
   let id;
   let shoppingCart = [];
+  let total = 0;
 
-//get the menu from API
+  $.get( "https://galvanize-eats-api.herokuapp.com/menu", function( data ) {
+    storeData(data);
+  });
 
-$.get( "https://galvanize-eats-api.herokuapp.com/menu", function( data ) {
-  storeData(data);
-});
-
-
-//store the data in a variable
-function storeData (data) {
-  menu = data;
-  divideCatagories(menu);
-  return menu;
-}
-
-
-//access the data using a foreach
-function divideCatagories(obj) {
-  item = obj.menu;
-  console.log('item',item);
-  item.forEach(subCat)
-  }
-var populate = new Promise (
-
-
-)
-//set variables from object
-  function subCat (item){
-    price = item.price;
-    name = item.name;
-    type = item.type;
-    id = item.id;
-    show(price,name,id);
-    console.log('subcat price',price);
+  function storeData (data) {
+    menu = data;
+    divideCatagories(menu);
+    return menu;
   }
 
-//add divs to window
-function show(price,name,id) {
-  $('#menuWindow').append('<button class="menuItem" data-price='+price+' data-name='+name+'>' + name + " " + price + '</button>')
-}
+  function divideCatagories(obj) {
+    item = obj.menu;
+    console.log('item',item);
+    item.forEach(subCat)
+    addevent()
+    }
+
+    function subCat (item){
+      price = item.price;
+      name = item.name;
+      type = item.type;
+      id = item.id;
+      show(price,name,id);
+      console.log('subcat price',price);
+    }
+
+    function show(price,name,id) {
+      $('#menuWindow').append('<button class="menuItem" data-price='+price+' data-name='+name+'>' + name + " " + price + '</button>')
+    }
 
 function priceCalc (name,price,quantity) {
   subTotal = price * quantity;
   shoppingCart.push([name,quantity,price,subTotal])
-  console.log('subTotal',shoppingCart);
+  return subTotal;
 }
-
-
 
 function addevent () {
 $('.menuItem').on('click',(function () {
+
   var orderedItemPrice = this.dataset.price;
   var orderedItemName = this.dataset.name;
   var quantityOrdered = $('#quantity').val()
   priceCalc(orderedItemName,orderedItemPrice,quantityOrdered)
+  totalCalc(orderedItemName,orderedItemPrice,quantityOrdered)
 
 }))
+}
+//sum the prices of the orders in the shopping cart
+function totalCalc () {
+  total = 0;
+  shoppingCart.forEach(function (item) {
+    total += item[3];
+    console.log('total',total);
+    return total;
+  })
+}
+
+
+$('#addItem').on('click',(function () {
+  event.preventDefault();
+  $('#orderDisp').empty()
+  shoppingCart.forEach((item) => {
+    $('#orderDisp').append('<div class="orderDispItem">' + item[0] + " x" + item[1] +'<div class="itemVal">' + "  $" + item[3] + '</div>' + '</div>')
+    console.log(item);
+  })
+
+}))
+
+
+$('#deliver').on('click', () => {
+  event.preventDefault();
+  checkout()
+})
+
+function checkout () {
+  console.log('shoppingCart',shoppingCart);
 }
 //access the price using the unique ID and prepare to sum
 
