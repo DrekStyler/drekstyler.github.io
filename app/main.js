cb71b890e8d61da67768a1b3526ffdf3459f4587
Derek Styer <dmstyer@gmail.com>
2016-08-22 22:54:24 -0600

    reattached sheets with test

--- app/main.js
+++ app/main.js
@@ -3,4 +3,5 @@ console.log('Am I sane?');
 //navigate to the orders page on button click
 $('#order').on('click',( ()=> {
   window.location.href = 'file:///Users/derekstyer/galvanize/unit-2/week1/galvanize-eats/app/pages/orders.html'
+  console.log('working');
 }))
