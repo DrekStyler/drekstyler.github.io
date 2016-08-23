f5aeff539d93cb8ccd7fb7cd669a6818876e3b81
Derek Styer <dmstyer@gmail.com>
2016-08-22 22:56:17 -0600

    changed file path

--- app/main.js
+++ app/main.js
@@ -2,6 +2,6 @@ console.log('Am I sane?');
 
 //navigate to the orders page on button click
 $('#order').on('click',( ()=> {
-  window.location.href = 'file:///Users/derekstyer/galvanize/unit-2/week1/galvanize-eats/app/pages/orders.html'
+  window.location.href = 'https://drekstyler.github.io/app/pages/orders.html'
   console.log('working');
 }))
